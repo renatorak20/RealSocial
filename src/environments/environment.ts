c3457162e715dc1848230e416cd21ea7e3b90631
Renato Rak <renatorak20@gmail.com>
2024-01-05 19:12:59 +0100

    init

---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBmkjA-lcVB9FYUTMtqVu05zNtgdJ-08kQ",
+    authDomain: "realsocial-77b47.firebaseapp.com",
+    projectId: "realsocial-77b47",
+    storageBucket: "realsocial-77b47.appspot.com",
+    messagingSenderId: "210247442913",
+    appId: "1:210247442913:web:86cceabe6db214f36672a0"
+  };
